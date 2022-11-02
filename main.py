6d678d868eaa34156f7cc07de3ab09679369cdeb
Benton Johnson <59421259+liondadev@users.noreply.github.com>
2022-11-02 12:09:26 -0400

    allow -er and -ir words

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def conjugate(word, starter):
             word = word + "o"
         elif starter == "usted" or starter == "el" or starter == "ella":
             word = word + "a"
-    return word
+    elif word.endswith("er") or word.endswith("ir"):
+        word = word.removesuffix("er")
+        word = word.removesuffix("ir")
+        if starter == "tu":
+            word = word + "es"
+        elif starter == "yo":
+            word = word + "o"
+        elif starter == "usted" or starter == "el" or starter == "ella":
+            word = word + "e"
+    return word # Return the new word, or the fallback old word
 
-print(conjugate("tomar", "usted"))
+# print(conjugate("tome", "usted"))
+while True:
+    _word = input("What word would you like to conjugate: ")
+    _pronoun = input("What pronoun should we use: ")
+    print(conjugate(_word, _pronoun))
